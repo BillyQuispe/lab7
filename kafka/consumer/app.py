980da3e347266c6b6acb2cf22fdb4fe1549cb61a
BillyQuispe <carlos.quispe.v@tecsup.edu.pe>
2024-09-29 23:00:44 -0500

    ultimas modificacione223s2222

--- kafka/consumer/app.py
+++ kafka/consumer/app.py
@@ -1,6 +1,6 @@
 from kafka import KafkaConsumer
 from flask import Flask, jsonify
-from flask_cors import CORS  # Importar CORS
+from flask_cors import CORS
 import json
 import threading
 
@@ -22,14 +22,22 @@ consumer = KafkaConsumer(
 def consume_messages():
     global music_data_list
     for message in consumer:
-        music_data_list.append(message.value)
-        print(f"Received: {message.value}")
+        # Suponiendo que el mensaje recibido es un diccionario con las claves 'a' y 't'
+        formatted_message = {
+            "a": message.value.get("artist"),  # Cambia 'artist' por la clave correcta en tu JSON
+            "t": message.value.get("title")    # Cambia 'title' por la clave correcta en tu JSON
+        }
+        music_data_list.append(formatted_message)  # Añade el mensaje formateado a la lista
+        print(f"Consuming messages from producer: {formatted_message}")  # Indica que está consumiendo un mensaje
+        print(f"Current music data list: {music_data_list}")  # Muestra la lista actual de datos de música
 
 @app.route('/api/music', methods=['GET'])
 def get_music():
-    return jsonify(music_data_list)
+    return jsonify(music_data_list)  # Devuelve la lista de música en formato JSON
 
 if __name__ == '__main__':
     # Inicia el hilo para consumir mensajes
     threading.Thread(target=consume_messages, daemon=True).start()
+    
+    # Ejecuta la aplicación Flask
     app.run(host='0.0.0.0', port=5002)
